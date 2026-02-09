6412de8b673849555e3024fb009a1a8a6c8a3750
anis dekik <anisdekik245@gmail.com>
2026-02-09 04:13:29 +0100

    add

--- js/accessGate.js
+++ js/accessGate.js
@@ -6,16 +6,7 @@ const AccessGate = {
     // Each user has a unique 6-character alphanumeric code (letters + numbers)
     USER_CODES: {
         'admin': 'Adm1n0',     // Admin user
-        'user1': 'Us3r01',     // User 1
-        'user2': 'C0de2X',     // User 2
-        'user3': 'Ux3R9Q',     // User 3
-        'user4': 'D3s1gn4',    // User 4
-        'user5': 'Cr8t0r5',    // User 5
-        'editor': 'Ed1t0R',    // Editor role
-        'moderator': 'M0d7r8', // Moderator role
-        'guest': 'Gu3sT0',     // Guest user
-        'tester': 'T3st99',    // Tester account
-
+        'Hichem': '120305',     // User 1
         // Add more users below:
         // 'username': 'CODE00',
     },
